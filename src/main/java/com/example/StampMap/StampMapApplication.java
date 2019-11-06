352c564a98ededa1e8e3ccfe991c31e673ffaa23
huangyingming <yellowhideaki@gmail.com>
2019-11-06 21:57:28 +0900

    initialize

---
+++ src/main/java/com/example/StampMap/StampMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.StampMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StampMapApplication.class, args);
+	}
+
+}
