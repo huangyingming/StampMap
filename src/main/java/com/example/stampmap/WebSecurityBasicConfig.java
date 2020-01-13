099bcebb3e1107defa0ff787e1288413095fac4e
Yingming <Yingming@192.168.0.16>
2020-01-13 11:44:17 +0900

    Implement basic authentication2

--- src/main/java/com/example/stampmap/WebSecurityBasicConfig.java
+++ src/main/java/com/example/stampmap/WebSecurityBasicConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityBasicConfig extends WebSecurityConfigurerAdapter  {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-          .withUser("user").password("{noop}password")
+          .withUser("user").password("{noop}loremipsum")
           .authorities("ROLE__USER");
     }
 
